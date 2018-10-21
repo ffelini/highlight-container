66eab167a49e559baea562e206d480c3a749702c
vturcan <valeriu87@gmail.com>
2018-10-22 01:03:19 +0300

    Implement highLightView basic functionality
    HLC-3

--- app/src/main/java/vturcan/com/highlightcontainer/MainActivity.kt
+++ app/src/main/java/vturcan/com/highlightcontainer/MainActivity.kt
@@ -2,12 +2,25 @@ package vturcan.com.highlightcontainer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import highlightcontainer.vturcan.com.highlight_container.R
+import vturcan.com.highligthcontainer.HighligthContainer
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settingsHighlightContainer = findViewById<HighligthContainer>(R.id.settings_highlight_container)
+        val emailHighlightContainer = findViewById<HighligthContainer>(R.id.email_highlight_container)
+        val settingsIcon = findViewById<View>(R.id.settings_icon).apply {
+            setOnClickListener { settingsHighlightContainer.highlightView(this) }
+        }
+        val emailIcon = findViewById<View>(R.id.email_icon).apply {
+            setOnClickListener { emailHighlightContainer.highlightView(this) }
+        }
     }
+
 }
--- highligthcontainer/src/main/java/vturcan/com/highligthcontainer/HighligthContainer.kt
+++ highligthcontainer/src/main/java/vturcan/com/highligthcontainer/HighligthContainer.kt
@@ -3,22 +3,39 @@ package vturcan.com.highligthcontainer
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.widget.RelativeLayout
+import android.widget.FrameLayout
 
 class HighligthContainer @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
-) : RelativeLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var source: View
+    private lateinit var highLightView: View
 
     init {
         context.theme.obtainStyledAttributes(attrs, R.styleable.HighligthContainer, 0, 0).apply {
             try {
                 val headerLayout = getResourceId(R.styleable.HighligthContainer_highlight_source_layout, 0)
-                View.inflate(context, headerLayout, this@HighligthContainer)
+                highLightView = View.inflate(context, headerLayout, this@HighligthContainer)
             } finally {
                 recycle()
             }
         }
+        visibility = View.GONE
+
+        setOnClickListener { visibility = View.GONE }
+    }
+
+    fun highlightView(source: View) {
+        this.source = source
+        highLightView.apply {
+            layoutParams = source.layoutParams
+            x = source.x
+            y = source.y
+        }
+
+        visibility = View.VISIBLE
     }
 }
