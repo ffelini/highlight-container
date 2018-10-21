ce1f0b9f249c6551399e3150a47a213493aebc44
vturcan <valeriu87@gmail.com>
2018-10-21 23:57:58 +0300

    Setup library module
    HLC-2

---
+++ app/src/main/java/vturcan/com/highlightcontainer/MainActivity.kt
@@ -0,0 +1,13 @@
+package vturcan.com.highlightcontainer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import highlightcontainer.vturcan.com.highlight_container.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ highligthcontainer/src/main/java/vturcan/com/highligthcontainer/HighligthContainer.kt
@@ -0,0 +1,16 @@
+package vturcan.com.highligthcontainer
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class HighligthContainer @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : RelativeLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+}
